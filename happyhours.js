bbf995c313db5b362d5f1a52de066f475f17c723
Quinn Rohlf <qr@qrohlf.com>
2016-08-20 09:08:43 -0700

    🍸 add initial dataset

---
+++ happyhours.js
@@ -0,0 +1,79 @@
+const happyhours = [
+  {
+    name: 'Bar Bar',
+    summary: 'Draft specials and $1 off all house cocktails. Good taste in music recommended.',
+    start: '16:00',
+    end: '18:00',
+    days: 'all'
+  },
+  {
+    name: 'Life of Pie Pizza',
+    summary: '$5 pizza, $4 pints, and $4 house wine. Sure, you could cook tonight. This is easier.',
+    start: '11:00',
+    end: '18:00',
+    days: 'all'
+  },
+  {
+    name: 'Crow Bar',
+    summary: '$1.50 PBR, $2.50 micros. Consider wearing leather.',
+    start: '15:00',
+    end: '20:00',
+    days: 'all'
+  },
+  {
+    name: 'The Box Social',
+    summary: '$3-6 appetizers and $4-$6 beers and cocktails. Classy as fuck.',
+    start: '16:00',
+    end: '18:00',
+    days: 'all'
+  },
+  {
+    name: 'The Box Social Late Night',
+    summary: '$3-6 appetizers and $4-$6 beers and cocktails. Classy as fuck.',
+    start: '23:00',
+    end: '2:00',
+    days: 'SuMTuWTh'
+  },
+  {
+    name: 'Sidebar',
+    summary: 'Stuff. For $5.',
+    start: '16:00',
+    end: '18:00',
+    days: 'WThFSa'
+  },
+  {
+    name: 'Hopworks Bikebar',
+    summary: '$3-5 sliders and appetizers. Yelling children complimentary.',
+    start: '15:00',
+    end: '18:00',
+    days: 'all'
+  },
+  {
+    name: 'Hopworks Bikebar Late Night',
+    summary: '$3-5 sliders and appetizers.',
+    start: '21:00',
+    end: '24:00',
+    days: 'all'
+  },
+  {
+    name: 'Ecliptic Brewing',
+    summary: 'Food is still stupid expensive, but at least they have a $3 porter float?',
+    start: '15:00',
+    end: '18:00',
+    days: 'SuTuWThFSa'
+  },
+  {
+    name: 'Ecliptic Brewing',
+    summary: 'Food is still stupid expensive, but at least they have a $3 porter float?',
+    start: '11:00',
+    end: '23:00',
+    days: 'M'
+  },
+  {
+    name: 'sidecar 11',
+    summary: '$5.50 appetizers with a rotating drink special. No capital letters anywhere on the menu.',
+    start: '16:00',
+    end: '19:00',
+    days: 'all'
+  }
+]
